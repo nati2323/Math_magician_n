5416d6ee0e2f738f9b8a799ab41678e8345f02e2
Natnael Amare <99729850+nati2323@users.noreply.github.com>
2022-09-23 18:28:25 +0300

    Update resultIcone.js

--- src/components/resultIcone.js
+++ src/components/resultIcone.js
@@ -1,3 +1,4 @@
-export const ResultIcone = (props) => {
-  return (<div className = "calculator-result"> <span> {props.total}</span> </div>); 
-}
+export const ResultIcone=(props) => {
+  return (<div className="calculator-result">
+     <span> {props.total}</span> </div>);
+};
