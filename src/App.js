5665d0f2e06b23a765038cfc6a667ed6becb53f8
Natnael Amare <99729850+nati2323@users.noreply.github.com>
2022-09-23 17:35:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import { CalculatorDesign } from "./components/Calculator";
-import { Homepage } from "./components/Home";
-import { Quotepage } from "./components/Quote";
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link
-} from "react-router-dom";
+  Link,
+ } from 'react-router-dom';
+import { CalculatorDesign } from './components/Calculator';
+import { Homepage } from './components/Home';
+import { Quotepage } from './components/Quote';
 function App() {
   const pageHandler = (e) => {
     const target = e.target;
     const lists = Array.from(document.querySelectorAll('li'));
-    lists.forEach( li => {
+    lists.forEach(li => {(
       const anchor = li.querySelector('a');
       anchor.style.color = 'rgb(133, 134, 147)';
-    })
+    });
     target.style.color = '#aaa';
-  }
+  };
   return (
     <Router>
-      <div className= "App" >
-        <nav className = "navClass">
+      <div className="App">
+        <nav className="navClass">
           <h1> Math Magicians </h1>
-         <ul>
-          <li >
-            <Link to = "/" onClick = {pageHandler}>Home</Link>
+          <ul>
+           <li>
+            <Link to="/" onClick={pageHandler}>Home</Link>
           </li>
-          <li >
-            <Link to = "/Calculator" onClick = {pageHandler}>Calculator</Link>
+           <li>
+            <Link to="/Calculator" onClick={pageHandler}>Calculator</Link>
           </li>
           <li>
-            <Link to = "/Quote" onClick = {pageHandler}>Quote</Link>
+            <Link to = "/Quote" onClick={pageHandler}>Quote</Link>
           </li>
          </ul>
         </nav>
         <Switch>
-          <Route exact path = "/">
+          <Route exact path="/">
             <Homepage />
           </Route>
-          <Route path = "/Calculator" >
+          <Route path="/Calculator">
             <CalculatorDesign />
           </Route>
-          <Route path = "/Quote">
+          <Route path="/Quote">
             <Quotepage />
           </Route>
         </Switch>
